c7111e74af35b8544decb29c68b715cb881798a5
Stanislav Zaleski <sigmaray@gmail.com>
2024-01-08 23:57:41 +0600

    .

--- 2.rb
+++ 2.rb
@@ -1,4 +1,6 @@
 require "gtk3"
+require "json"
+require "./state.rb"
 
 window = Gtk::Window.new("First example")
 window.set_size_request(400, 400)
@@ -12,24 +14,37 @@ window.set_border_width(10)
 # window.add(button)
 
 
-text_view = Gtk::TextView.new
-window.add(text_view)
+$view1 = Gtk::TextView.new
+window.add($view1)
 
-window.signal_connect("key-press-event") do |_widget, event|
-  # Gdk::Keyval::name
+window.signal_connect("key-press-event") { |_widget, event|
   # p event.inspect
-  # Gdk::Keyval
-  # p event.keyval
-  # p Gdk::Keyval.to_name(event.keyval)
-  text_view.buffer.set_text(Gdk::Keyval.to_name(event.keyval))
-  text_view.set_editable(false)
-  if ["Up", "Down", "Left", "Right"].include?(Gdk::Keyval.to_name(event.keyval))
-    p 'in arrows'
-  end
-end
+  k = Gdk::Keyval.to_name(event.keyval)
+    if k == 'Up'
+      state_snake_up($state)
+    elsif k == 'Down'
+      state_snake_down($state)
+    elsif k == 'Left'
+      state_snake_left($state)
+    elsif k == 'Right'
+      state_snake_right($state)
+    end
+  replace_text(
+    # event.inspect    
+    Gdk::Keyval.to_name(event.keyval) +
+    "\n\n" +
+    # state_to_matrix($state).to_json
+    state_to_boad_string($state)
+  )
+}
 
 window.signal_connect("delete-event") { |_widget| Gtk.main_quit }
 
 window.show_all
 
+def replace_text(str)
+  $view1.buffer.text = str
+end
+
 Gtk.main
+
--- state.rb
+++ state.rb
@@ -1,6 +1,12 @@
+require 'yaml'
+
 MATRIX_SIZE = 10
 
-state = {
+MATRIX_TYPE_TAIL = 't'
+MATRIX_TYPE_HEAD = 'h'
+MATRIX_TYPE_FOOD = 'f'
+
+$state = {
   segments: [
     {x: 1, y: 1}
   ],
@@ -8,11 +14,68 @@ state = {
 }
 
 def state_to_matrix(state)
+  matrix = []
   MATRIX_SIZE.times do |y|
+    matrix[y] = []
     MATRIX_SIZE.times do |x|
-      p ({x:, y:})
+      # p ({x:, y:})
+      matrix[y][x] = '_'
     end
+  end
+  if !state[:food].nil?
+    matrix[state[:food][:y]][state[:food][:x]] = MATRIX_TYPE_FOOD
+  end
+  state[:segments].each_with_index do |segment, i|
+    matrix[segment[:y]][segment[:x]] = (i == 0) ? MATRIX_TYPE_HEAD : MATRIX_TYPE_TAIL
+  end
+  return matrix
+end
+
+def state_to_boad_string(state)
+  matrix = state_to_matrix(state)
+  return if matrix.length == 0
+  out = ''  
+  matrix.each do |row|
+    out += "["
+    row.each do |segment|
+      out += segment + ' '
+    end
+    out += "]\n"
+  end
+  
+  out
+end
+
+def state_snake_up(state) 
+  if state[:segments][0][:y] > 0
+    state[:segments][0][:y] -= 1    
+  else
+    state[:segments][0][:y] = MATRIX_SIZE - 1    
   end
 end
 
-state_to_matrix(state)
+def state_snake_down(state) 
+  if state[:segments][0][:y] < MATRIX_SIZE - 1
+    state[:segments][0][:y] += 1    
+  else
+    state[:segments][0][:y] = 0
+  end
+end
+
+def state_snake_left(state) 
+  if state[:segments][0][:x] > 0
+    state[:segments][0][:x] -= 1    
+  else
+    state[:segments][0][:x] = MATRIX_SIZE - 1    
+  end
+end
+
+def state_snake_right(state) 
+  if state[:segments][0][:x] < MATRIX_SIZE - 1
+    state[:segments][0][:x] += 1    
+  else
+    state[:segments][0][:x] = 0
+  end
+end
+
+print state_to_matrix($state).to_yaml
